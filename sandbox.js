e6006a300f7cc8bf4bb265685f706286ddbbbebc
Nicole Avila <avila.nicky89@gmail.com>
2023-12-30 15:36:17 +0100

    fixed the comment box

--- sandbox.js
+++ sandbox.js
@@ -1,55 +1,53 @@
 const btn = document.getElementById('submit')
 const nameBox = document.getElementById('name-input')
 const comment = document.getElementById('comment')
-const commentCounter= document.getElementById("counter")
+const commentCounter = document.getElementById("counter")
 const whoPosted = document.getElementById('date-and-who')
 const boxNewComment = document.getElementById('comments-added')
 const newParagrah = document.createElement('p')
-//array to store the comments
-let commentBox = [];
-//counter for the comment
-let count= 0;
 
-function clicker(){
-   addingComments()
-   displayComment()
-   createNewElement()
-   commentCounter.innerHTML = commentBox.length + ' Comments'
+//counter for the comment
+let count = 1;
+
+function addingComments() {
+      var post_div = document.getElementById("written-comments");
+      var content = document.createElement("p");
+      content.innerHTML = comment.value
+      post_div.appendChild(content)
+      post_div.appendChild(document.createElement("br"))
+         commentCounter.innerHTML = count + ' Comments';
+   count++;
 };
 
-function addingComments(){
-   commentBox.push(comment.value)
-}
 
-function displayComment(){
+function displayComment() {
    boxNewComment.innerHTML = commentBox[count];
-   boxNewComment.style.display ='block';
-   count++;
+   boxNewComment.style.display = 'block';
+   
 }
 // const button = document.createElement('button')
 // button.id= `${post.id}`
 // button.innerText = 'Leave a Comment'
 // postContainer.appendChild(button)
 
-//  function createNewElement(){
-// const newP= document.createElement('p');
-// newP.innerHTML = commentBox[count]
-// boxNewComment.appendChild('p')
-
-// }
+function createNewElement() {
+   const newP = document.createElement('p');
+   const newComment = commentBox[count]
+   newP.appendChild(newComment)
+}
 //keeping track of the date posted
-let myDate= new Date()
+let myDate = new Date()
 
-let myYear= myDate.getFullYear()
-let myMonth= myDate.getMonth()
-let myDay= myDate.getDay()-1
+let myYear = myDate.getFullYear()
+let myMonth = myDate.getMonth()
+let myDay = myDate.getDay() - 1
 
-let months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-let days=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 console.log(months)
 console.log(myMonth)
 
-function getDate(){
+function getDate() {
    console.log(`posted on ${days[myDay]} ${myDay}, ${months[myMonth]}, ${myYear}`)
 }
 
